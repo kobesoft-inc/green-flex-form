310dc62c5af85268c8bfd2138cd2ea7c31d3bf92
松岡 利昌 <t.matsuoka@kobesoft.co.jp>
2025-01-13 17:09:29 +0900

    英語の翻訳データ

---
+++ lang/en/flex-form.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // Designer Form
+    'actions' => [
+        'add-field' => 'Add Field',
+    ],
+
+    // Modal
+    'modal' => [
+        'heading' => 'Field Settings',
+        'update' => 'Update',
+    ],
+
+    // Fields
+    'fields' => [
+        // Field
+        'field' => [
+            'label' => 'Label',
+            'description' => 'Description',
+            'placeholder' => 'Placeholder',
+            'required' => 'Required',
+            'options' => 'Options',
+        ],
+
+        // Text Input
+        'text-input' => [
+            'label' => 'Single Line Text',
+        ],
+
+        // Textarea
+        'textarea' => [
+            'label' => 'Paragraph Text',
+        ],
+
+        // Dropdown
+        'select' => [
+            'label' => 'Dropdown',
+        ],
+
+        // Radio Button
+        'radio' => [
+            'label' => 'Radio Button',
+        ],
+
+        // Checkbox
+        'checkbox' => [
+            'label' => 'Checkbox',
+        ],
+
+        // File Upload
+        'file-upload' => [
+            'label' => 'File Upload',
+        ],
+
+        // Date Picker
+        'date-picker' => [
+            'label' => 'Date',
+        ],
+
+        // Time Picker
+        'time-picker' => [
+            'label' => 'Time',
+        ],
+    ]
+];
